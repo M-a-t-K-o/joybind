c79343c4dfcbb8e0b2bd5ee6a78330341819889b
Mat-Ko <matejponik@proton.me>
2024-10-23 10:48:45 +0200

    maintenance

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1728,
+        height: 972,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+return (
+<div className="header">
+    <h1>aaaaaaaaa</h1>
+</div>
+);
+}
+
+export default App;
